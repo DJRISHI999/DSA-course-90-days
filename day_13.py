8eac6d01fa1ffd29fb6519d7f17fbab8fd0323f3
Dhruv Kumar Jaiswal <rishidhruv999@gmail.com>
2026-01-17 21:14:11 +0530

    balanced Paranthesis

--- day_13.py
+++ day_13.py
@@ -37,3 +37,56 @@ def reverseString(string):
         return reversed
 
 print(reverseString("Neelu"))
+
+
+# Q2. Check for balanced parentheses in an expression
+
+class Stack:
+    def __init__(self,string):
+        self.size = len(string)
+        self.stack = [0]*self.size
+        self.top = -1
+        self.string = string
+
+
+    def push(self):
+        if self.top == self.size-1:
+            return
+        self.top += 1
+        self.stack[self.top] = self.string[self.top]
+
+
+    def pop(self):
+        if self.top == -1:
+            return
+        value = self.stack[self.top]
+        self.top = self.top-1
+        return value
+    
+
+def isBalanced(string):
+    pair = {
+        "{": "}",
+        "[" : "]",
+        "(": ")"
+    }
+
+    if len(string) % 2 != 0:
+        return False
+    else:
+        abc = Stack(string)
+        for i in range(abc.size // 2):
+            abc.push()
+
+        mid = abc.size // 2 - 1
+
+        for j in range(mid+1,abc.size):
+            char = abc.pop()
+            if pair.get(char) != string[j]:
+                return False
+        return True
+    
+
+print(isBalanced("{[()]}"))  # True
+print(isBalanced("{[(])}"))  # False
+print(isBalanced("{{[[(())]]}}"))  # True

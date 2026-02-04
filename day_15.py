d1dfcc3c9ba85e96a30f6fff8223db52688371af
Dhruv Kumar Jaiswal <rishidhruv999@gmail.com>
2026-02-04 23:57:48 +0530

    fast & Slow pointer linkedList

--- day_15.py
+++ day_15.py
@@ -41,6 +41,7 @@ class LinkedList:
             temp = temp.next
 
         temp.next = new_node
+        new_node.next = self.head
 
 
     def insertAtPosition(self,data,pos):
@@ -119,27 +120,133 @@ class LinkedList:
         value = temp.next
         temp.next = temp.next.next
         return value
+    
+    def find_middle(self):
+        slow = self.head
+        fast = self.head
+
+        while fast is not None and fast.next is not None:
+            slow = slow.next
+            fast = fast.next.next
+
+        return slow.data
+    
+
+    def find_cycle(self):
+        slow = self.head
+        fast = self.head
+
+        while fast is not None and fast.next is not None:
+            slow = slow.next
+            fast = fast.next.next
+
+            if slow == fast:
+                return "Cycle Found !!!!"
+            
+        return "Cycle not found"
+    
+
+    def find_cycle_start_point(self):
+        slow = self.head
+        fast = self.head
+        # detect cycle 
+        while fast is not None and fast.next is not None:
+            slow = slow.next
+            fast = fast.next.next
+
+            if slow == fast:
+                break # instead of returning that cycle is detected we will break loop and let the pointers be there 
+
+        else: 
+            return None # this is when we couldn't detect cycle
+        
+
+        slow = self.head # move slow pointer to head and fast pointer will be there at meet point
+
+        while slow != fast:
+            slow = slow.next
+            fast = fast.next
+
+        return slow.data
+    
+
+    def loop_length(self):
+        slow = self.head
+        fast = self.head
+        # detect cycle 
+        while fast is not None and fast.next is not None:
+            slow = slow.next
+            fast = fast.next.next
+
+            if slow == fast:
+                break # instead of returning that cycle is detected we will break loop and let the pointers be there 
+
+        else: 
+            return None # this is when we couldn't detect cycle
+        
+
+        slow = self.head # move slow pointer to head and fast pointer will be there at meet point
+
+        while slow != fast:
+            slow = slow.next
+            fast = fast.next
+
+        fast = fast.next
+        length = 1
+        while slow != fast:
+            fast = fast.next
+            length += 1
+
+        return length
+        
 
 
 ll = LinkedList()
 
-ll.insertEnd(20)
-ll.insertEnd(30)
-
 ll.insertAtBegin(10)
+
+ll.insertEnd(20)
 
 ll.insertAtPosition(15,3)
 
-ll.display()
+# ll.display()
 
-ll.deleteAtPosition(2)
-ll.deleteEnd()
-ll.deleteStart()
+# print(ll.find_middle())
 
-ll.display()
+print(ll.find_cycle())
+print(ll.find_cycle_start_point())
+print(ll.loop_length())
+
+# ll.deleteAtPosition(2)
+# ll.deleteEnd()
+# ll.deleteStart()
+
+# ll.display()
 
 
 
+# Fast and Slow pointer :
 
-# Deletion from LinkedList
+# ham head se start krenge dono ko 
+#  dono aage badte jayenge 
+# Fast wala thoda fast aage badega maan lo 2 steps at one time and Slow wala dheere dheere badega maan lo 1 step at a time 
+
+# ideally singly linked list me fast poninter hmesha aage rhega Slow pointer ke.
+# but agr linkedlist ke andr cycle present h to Fast pointer kabhi na kabhi Slow pointer ko catch kr lega and tabhi hame pata chal jayega ki cycle present h
+
+# Use cases:
+'''
+1. Middle of LinkedList dhundh skte h 
+2. Cycle find krna
+3. Cycle ka starting point
+4. Palindrome
+5. loop length 
+'''
+
+
+
+# palindrome...
+
+
+
 

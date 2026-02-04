baabeb76470a89bf8768eea347da31247be55b00
Dhruv Kumar Jaiswal <rishidhruv999@gmail.com>
2026-02-04 22:06:24 +0530

    deletion of linkedList

--- day_15.py
+++ day_15.py
@@ -72,6 +72,53 @@ class LinkedList:
 
         print("None")
 
+    def deleteStart(self):
+        if self.head is None:
+            print("No element in LL")
+            return 
+        
+        temp = self.head
+        self.head = temp.next
+
+        return self.head
+    
+
+    def deleteEnd(self):
+        if self.head is None:
+            print("No element in LL")
+            return 
+        temp = self.head
+        while temp.next.next is not None:
+            temp = temp.next
+
+        value = temp.next
+        temp.next = None
+
+        return value
+    
+
+    def deleteAtPosition(self,pos):
+        if self.head is None:
+            print("No element in LL")
+            return 
+        
+        if pos == 0:
+            self.deleteStart()
+
+        temp = self.head
+        i = 0
+        while temp.next is not None and i < pos-1:
+            temp = temp.next
+            i += 1
+
+
+        if  temp.next is None:
+            print("position out of range")
+            return
+        
+        value = temp.next
+        temp.next = temp.next.next
+        return value
 
 
 ll = LinkedList()
@@ -81,7 +128,18 @@ ll.insertEnd(30)
 
 ll.insertAtBegin(10)
 
-ll.insertAtPosition(15,4)
+ll.insertAtPosition(15,3)
 
 ll.display()
+
+ll.deleteAtPosition(2)
+ll.deleteEnd()
+ll.deleteStart()
+
+ll.display()
+
+
+
+
+# Deletion from LinkedList
 
